f28292494abd47365e68d638309a22576ad507cc
Noel George <70696198+ng2027@users.noreply.github.com>
2026-02-18 21:33:50 -0500

    setup (#10)

---
+++ frontend/popup.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return (
+    <div
+      style={{
+        padding: 16
+      }}>
+      Focus
+    </div>
+  )
+}
+
+export default IndexPopup
